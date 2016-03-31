60276d20588b5367db4836503bad11f6be366658
endika <ealdecoa004@ikasle.ehu.es>
2016-04-01 00:34:22 +0200

    Update Main.java
    etorkizunerako laguntza

--- MoviesReviewsClassifier/src/classify/Main.java
+++ MoviesReviewsClassifier/src/classify/Main.java
@@ -1,5 +1,5 @@
 package classify;
-
+//https://stackoverflow.com/questions/20017957/how-to-reuse-saved-classifier-created-from-explorerin-weka-in-eclipse-java
 public class Main {
 
 	public static void main(String[] args) {
