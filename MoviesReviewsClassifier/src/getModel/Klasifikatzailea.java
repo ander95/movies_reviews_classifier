e29c6f092576f45f6bd679662b83a84bcf6a3351
endikap100 <ealdecoa004@ikasle.ehu.es>
2016-04-01 00:49:55 +0200

    start with bagging

---
+++ MoviesReviewsClassifier/src/getModel/Klasifikatzailea.java
@@ -0,0 +1,35 @@
+package getModel;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.meta.Bagging;
+import weka.core.Instances;
+import weka.core.SerializationHelper;
+
+public class Klasifikatzailea {
+
+	private Bagging klasifikatzailea;
+	public Klasifikatzailea(Instances data, int numIterations){
+		try {
+			klasifikatzailea.setSeed(1);
+			klasifikatzailea.setNumIterations(numIterations);
+			//TODO
+			String[] optionsBagging = {};
+			klasifikatzailea.setOptions(optionsBagging);
+			klasifikatzailea.buildClassifier(data);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public Classifier getClassifier(){
+		return klasifikatzailea.getClassifier();
+	}
+	public void save(String path){
+		try {
+			SerializationHelper.write(path, getClassifier());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
