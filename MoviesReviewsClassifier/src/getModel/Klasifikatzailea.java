d8dc42ec6c0b65803efaad444f5a0a311b8810ed
endikap100 <ealdecoa004@ikasle.ehu.es>
2016-04-01 00:51:52 +0200

    start with getModel

--- MoviesReviewsClassifier/src/getModel/Klasifikatzailea.java
+++ MoviesReviewsClassifier/src/getModel/Klasifikatzailea.java
@@ -29,7 +29,7 @@ public class Klasifikatzailea {
 			SerializationHelper.write(path, getClassifier());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e.printStackTrace(); 
 		}
 	}
 }
