02d2631af41f229ae9d2730c90c20698882ecb0e
antton95 <aojanguren004@ikasle.ehu.es>
2016-04-07 11:26:11 +0200

    parametro ekorketa

--- MoviesReviewsClassifier/src/getModel/Klasifikatzailea.java
+++ MoviesReviewsClassifier/src/getModel/Klasifikatzailea.java
@@ -1,6 +1,8 @@
 package getModel;
 
+import java.util.Random;
 import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
 import weka.classifiers.meta.Bagging;
 import weka.core.Instances;
 import weka.core.SerializationHelper;
@@ -30,6 +32,32 @@ public class Klasifikatzailea {
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace(); 
+		}
+	}
+
+	public void parametroEkorketa(Instances trainData) {
+		Bagging est=(Bagging) this.getClassifier();
+		int kMax=0;
+		Evaluation evaluator=null;
+		double fMeasureMax=0;
+		double fMeasure=0;
+		for (int k = 1; k < 20; k++) {
+			est.setNumIterations(k);
+			for (int i = 1; i < 100; i++) {
+				try {
+					evaluator = new Evaluation(trainData);
+					evaluator.crossValidateModel(est, trainData, 10, new Random(1));
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				fMeasure=evaluator.fMeasure(trainData.numAttributes());
+				//			fMeasure=evaluator.fMeasure(0);
+				if (fMeasureMax <fMeasure){
+					kMax=k;
+					fMeasureMax=fMeasure;
+				}
+			}
 		}
 	}
 }
