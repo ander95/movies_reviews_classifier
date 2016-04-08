dfe175d8d74dcbd61a55be4c4670b544e7eb2665
ander95 <auribarri011@ikasle.ehu.es>
2016-04-08 12:02:16 +0200

    Added files via upload
    preprocess a

--- MoviesReviewsClassifier/src/preprocess/DataFilters.java
+++ MoviesReviewsClassifier/src/preprocess/DataFilters.java
@@ -15,7 +15,6 @@ public class DataFilters {
 
 	}
 
-
 	private Instances applyFilter(Filter filter, Instances data) {
 		try {
 			filter.setInputFormat(data);
@@ -60,8 +59,7 @@ public class DataFilters {
 			bow.setTFTransform(true);
 			bow.setLowerCaseTokens(true);
 			bow.setOutputWordCounts(true);
-			bow.setAttributeIndices("1");
-			bow.setWordsToKeep(2000);
+			bow.setWordsToKeep(450);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -69,18 +67,32 @@ public class DataFilters {
 	}
 
 
-	public Instances getGainAttributeEval(Instances data){
+	public Instances[] getGainAttributeEval(Instances train, Instances test){
 
+		Instances[] inst = new Instances[2];
+		
 		AttributeSelection filter = new AttributeSelection();
 		InfoGainAttributeEval eval = new InfoGainAttributeEval();
 		Ranker search = new Ranker();
-
 		search.setThreshold(0.0);
 		filter.setEvaluator(eval);
 		filter.setSearch(search);
+		
+		Instances newTrain = null;
+		Instances newTest = null;
+		
+		try {
+			filter.setInputFormat(train);
+			newTrain = Filter.useFilter(train, filter);
+			newTest = Filter.useFilter(test, filter);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		inst[0] = newTrain;
+		inst[1] = newTest;
 
-
-		return applyFilter(filter, data);
+		return inst;
 	}
 
 
